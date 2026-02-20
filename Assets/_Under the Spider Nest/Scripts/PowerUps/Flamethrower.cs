0a4efcf71d4f01e56f2ba3ceba2e4a3eada7f277
Alejandra05gsr <79392781+Alejandra05gsr@users.noreply.github.com>
2026-02-20 08:38:05 -0600

    Avances y mejoras de powerups

--- Assets/_Under the Spider Nest/Scripts/PowerUps/Flamethrower.cs
+++ Assets/_Under the Spider Nest/Scripts/PowerUps/Flamethrower.cs
@@ -3,27 +3,43 @@ using UnityEngine;
 public class Flamethrower : Weapons
 {
 
-    public ParticleSystem insecticide;
+    public GameObject insecticide;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        insecticide.gameObject.GetComponent<ParticleSystem>().Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!gameObject.activeSelf) return;
+
         if (Input.GetMouseButton(0))
         {
-            ShootBehaviour();
+                ShootBehaviour();
+        }
+        else
+        {
+            insecticide.gameObject.GetComponent<ParticleSystem>().Stop();
         }
     }
 
 
     protected override void ShootBehaviour()
     {
-        insecticide.Play();
+        insecticide.gameObject.GetComponent<ParticleSystem>().Play();
+        Debug.Log("Insecticida activo");
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            Debug.Log("Enemigo alcanzado por el insecticida");
+            other.GetComponent<EnemyHealth>().TakeDamage(1);
+        }
     }
 
 }
